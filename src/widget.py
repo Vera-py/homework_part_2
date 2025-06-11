7f6b87c44ac1096861cfd0337347b52774a26cae
Vera <vk.1701@yandex.ru>
2025-06-11 12:37:35 +0500

    small fixes masks and widget

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(account_card: str) ->str:
     """Функция определяет введен номер счета или карты и маскирует соответственно данным"""
     if "Счет" in account_card:
